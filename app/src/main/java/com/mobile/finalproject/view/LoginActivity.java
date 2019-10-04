2f353e558363702852cc0a487c7135bfad5a921b
Kevin Tran <kvtran@email.uark.edu>
2019-10-04 13:54:01 -0500

    Authentication Registering

---
+++ app/src/main/java/com/mobile/finalproject/view/LoginActivity.java
@@ -0,0 +1,29 @@
+package com.mobile.finalproject.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.mobile.finalproject.R;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.btn_register).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_register:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/mobile/finalproject/view/RegisterActivity.java
@@ -0,0 +1,93 @@
+package com.mobile.finalproject.view;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.mobile.finalproject.R;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editTextEmail, editTextPass;
+    ProgressBar progressSpinner;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        editTextEmail = findViewById(R.id.email_input);
+        editTextPass = findViewById(R.id.password_input);
+        progressSpinner = (ProgressBar) findViewById(R.id.progressBar);
+        mAuth = FirebaseAuth.getInstance();
+        findViewById(R.id.btn_register).setOnClickListener(this);
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String pass = editTextPass.getText().toString().trim();
+
+        if(email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            editTextEmail.setError("Please enter a valid email.");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if(pass.isEmpty()) {
+            editTextPass.setError("Password is required.");
+            editTextPass.requestFocus();
+            return;
+        }
+
+        if(pass.length() < 6) {
+            editTextPass.setError("Minimum length of password should is 6.");
+            editTextPass.requestFocus();
+            return;
+        }
+
+        progressSpinner.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(email,pass)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressSpinner.setVisibility(View.GONE);
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(),"User successfully registered", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getApplicationContext(),"Something went wrong.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btn_register:
+                registerUser();
+                break;
+        }
+    }
+
+
+}
