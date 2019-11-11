e2d96bba3d4a11e13506396654fd83774a203069
Alejandro Torrico <a.torricomoreno@gmail.com>
2019-11-10 20:39:41 -0600

    Started Navegation Drawer

--- app/src/main/java/com/mobile/finalproject/view/LoginActivity.java
+++ app/src/main/java/com/mobile/finalproject/view/LoginActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.mobile.finalproject.HomeActivity;
 import com.mobile.finalproject.R;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
@@ -16,6 +17,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_login);
 
         findViewById(R.id.btn_register).setOnClickListener(this);
+        findViewById(R.id.btn_login).setOnClickListener(this);
     }
 
     @Override
@@ -23,6 +25,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         switch (v.getId()) {
             case R.id.btn_register:
                 startActivity(new Intent(this, RegisterActivity.class));
+                break;
+            case R.id.btn_login:
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
                 break;
         }
     }
