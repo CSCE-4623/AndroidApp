cc39b3499662dced257a1d7dc3641b55694dbb6d
Alejandro Torrico <a.torricomoreno@gmail.com>
2019-11-12 18:25:16 -0600

    recycler view with Firebase

---
+++ app/src/main/java/com/mobile/finalproject/model/Products.java
@@ -0,0 +1,103 @@
+package com.mobile.finalproject.model;
+
+public class Products {
+
+    private String name;
+    private String description;
+    private long price;
+    private long quantity;
+    private String fileLocation;
+    private String Category;
+    private long itemID;
+    private String reservationStart;
+    private String reservationEnd;
+
+    public Products()
+    {
+
+    }
+
+    public Products(String name, String description, long price, long quantity, String fileLocation, String category, long itemID, String reservationStart, String reservationEnd) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.fileLocation = fileLocation;
+        this.Category = category;
+        this.itemID = itemID;
+        this.reservationStart = reservationStart;
+        this.reservationEnd = reservationEnd;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(long quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getFileLocation() {
+        return fileLocation;
+    }
+
+    public void setFileLocation(String fileLocation) {
+        this.fileLocation = fileLocation;
+    }
+
+    public String getCategory() {
+        return Category;
+    }
+
+    public void setCategory(String category) {
+        this.Category = category;
+    }
+
+    public long getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(long itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getReservationStart() {
+        return reservationStart;
+    }
+
+    public void setReservationStart(String reservationStart) {
+        this.reservationStart = reservationStart;
+    }
+
+    public String getReservationEnd() {
+        return reservationEnd;
+    }
+
+    public void setReservationEnd(String reservationEnd) {
+        this.reservationEnd = reservationEnd;
+    }
+}
---
+++ app/src/main/java/com/mobile/finalproject/viewHolder/ProductViewHolder.java
@@ -0,0 +1,40 @@
+package com.mobile.finalproject.viewHolder;
+
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mobile.finalproject.R;
+import com.mobile.finalproject.interfaces.ItemClickListener;
+
+public class ProductViewHolder  extends RecyclerView.ViewHolder implements View.OnClickListener
+{
+
+    public TextView txtProductName, txtProductDescription, txtProductPrice;
+    public ImageView imageView;
+    public ItemClickListener clickListener;
+
+    public ProductViewHolder(@NonNull View itemView) {
+        super(itemView);
+        imageView = (ImageView) itemView.findViewById(R.id.product_image);
+        txtProductName = (TextView) itemView.findViewById(R.id.product_name);
+        txtProductDescription = (TextView) itemView.findViewById(R.id.product_description);
+        txtProductPrice = (TextView) itemView.findViewById(R.id.product_price);
+
+        Log.i("ProductViewHolder", "Output: " + txtProductName + "  "  + txtProductDescription +
+                "  " + "Price/hr = " + txtProductPrice + "$");
+    }
+
+    public void setItemClickListener(ItemClickListener listener){
+        this.clickListener = listener;
+    }
+
+    @Override
+    public void onClick(View view) {
+        clickListener.onClick(view, getAdapterPosition(), false);
+    }
+}
