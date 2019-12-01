d1baa18c583ebc51437fc1d37d05cc35ed851e66
githubnotunix <githubnotunix@github.com>
2019-11-30 22:31:23 -0600

    red theme and add cart button

--- app/src/main/java/com/mobile/finalproject/ProductDetailsActivity.java
+++ app/src/main/java/com/mobile/finalproject/ProductDetailsActivity.java
@@ -6,8 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cepheuen.elegantnumberbutton.view.ElegantNumberButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,6 +21,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.mobile.finalproject.R;
 import com.mobile.finalproject.model.Products;
+import com.mobile.finalproject.view.CartActivity;
 import com.squareup.picasso.Picasso;
 
 public class ProductDetailsActivity extends AppCompatActivity {
@@ -26,6 +30,8 @@ public class ProductDetailsActivity extends AppCompatActivity {
     private ImageView productImage;
     private ElegantNumberButton numberButton;
     private TextView productPrice, productDescription, productName;
+    private Button cartButton;
+
     //private String productID = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +39,32 @@ public class ProductDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_product_details);
         Intent intent = getIntent();
         String productID = intent.getStringExtra(HomeActivity.ITEM_ID);
-        addToCartBtn = (FloatingActionButton) findViewById((R.id.add_product_to_cart_btn));
+        addToCartBtn = (FloatingActionButton) findViewById((R.id.cart_button));
         productImage = (ImageView) findViewById(R.id.product_image_details);
         numberButton = (ElegantNumberButton) findViewById(R.id.number_btn);
         productPrice = (TextView) findViewById(R.id.product_price_details);
         productDescription = (TextView) findViewById(R.id.product_description_details);
         productName = (TextView) findViewById(R.id.product_name_details);
+        cartButton = (Button) findViewById(R.id.add_to_cart_button);
+        addToCartBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ProductDetailsActivity.this, CartActivity.class);
+                startActivity(intent);
+            }
+        });
+        cartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(ProductDetailsActivity.this, HomeActivity.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "Product has been added", Toast.LENGTH_SHORT).show();
+            }
+        });
         getProductDetails(productID);
+
     }
     private void getProductDetails(String productID){
         DatabaseReference productsRef = FirebaseDatabase.getInstance().getReference().child("items");
